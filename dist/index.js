8c159eaa051b8c9845e5439c62895812018adeb9
Lubomir Lenard <lubomir.lenard@ness.com>
2022-11-07 12:30:29 +0100

    url shortener structure

--- dist/index.js
+++ dist/index.js
@@ -6,22 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
+const monk_1 = __importDefault(require("monk"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.static('dist/client'));
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+// DB connect (urlShortener)
+dotenv_1.default.config();
+const db = (0, monk_1.default)(process.env.MONGO_URL || '');
+const urls = db.get('urls');
+// Routing
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
     res.sendFile(path_1.default.resolve(__dirname, 'client', 'index.html'));
 });
-const monk_1 = __importDefault(require("monk"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const db = (0, monk_1.default)(process.env.MONGO_URL || '');
-const urls = db.get('urls'); // add type and so on
-console.log('MDBU', process.env.MONGO_URL);
-//console.log('urls', urls);
+app.get('/test', (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.write('test');
+    console.log('test');
+});
+// Server init 
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import path from 'path';
+import monk from 'monk';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,25 +11,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('dist/client'));
 
-app.listen(port, (): void => {
-  console.log(`Listening on port ${ port }`);
-})
+// DB connect (urlShortener)
+dotenv.config();
+const db = monk(process.env.MONGO_URL || '');
+const urls = db.get('urls');
 
+// Routing
 app.get('/', (req: Request, res: Response): void  => {
   res.set('Content-Type', 'text/html');
   res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
 
-/// remove
-import monk from 'monk';
-import dotenv from 'dotenv';
+app.get('/test', (req: Request, res: Response): void  => {
+  res.set('Content-Type', 'text/html');  
+  res.write('test');
+  urls.insert({ url: 'https://yahoo.com', short: 'ksdhf' })
+});
 
-dotenv.config();
-
-const db = monk(process.env.MONGO_URL || '');
-
-const urls = db.get('urls'); // add type and so on
-
-console.log('MDBU', process.env.MONGO_URL);
-
-//console.log('urls', urls);
+// Server init 
+app.listen(port, (): void => {
+  console.log(`Listening on port ${ port }`);
+})
